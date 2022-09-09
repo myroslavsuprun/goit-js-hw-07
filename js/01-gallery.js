0c92998a2de7c81f0eabd0411e97d2ecf160894a
MiroProger <sirmiroslavsuprun@gmail.com>
2022-09-09 13:57:54 +0300

    Task 1: added markUp and eventListener for the gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ref = {
+  galleryDiv: document.querySelector(".gallery"),
+};
+
+function galleryItemsMarkUp(gallery) {
+  return gallery
+    .map((galleryItem) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+ref.galleryDiv.insertAdjacentHTML(
+  "beforeend",
+  galleryItemsMarkUp(galleryItems)
+);
+
+ref.galleryDiv.addEventListener("click", onPictureResizeClick);
+
+function onPictureResizeClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const galleryLink = e.currentTarget.querySelector(".gallery__link");
+  console.log(galleryLink.href);
+}
